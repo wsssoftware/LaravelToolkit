e56f6b2cbbcca619d843263632c49619a384638b
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2024-09-03 17:05:32 -0300

    wip

---
+++ tests/FlashTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Collection;
+use LaravelToolkit\Facades\Flash;
+use LaravelToolkit\Flash\Message;
+
+it('can send flash', function () {
+    Flash::success('ok');
+    Flash::info('ok');
+    Flash::warn('ok');
+    Flash::error('ok');
+    Flash::secondary('ok');
+    Flash::contrast('ok');
+
+    expect(Flash::getMessages())
+        ->toHaveCount(6)
+        ->toBeInstanceOf(Collection::class)
+        ->each
+        ->toBeInstanceOf(Message::class);
+});
