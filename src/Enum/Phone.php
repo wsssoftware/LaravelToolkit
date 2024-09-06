b23b87588e2a2d3ef8ef72336e8bbb2f006521dd
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2024-09-06 20:52:26 -0300

    wip

--- src/Enum/Phone.php
+++ src/Enum/Phone.php
@@ -3,10 +3,12 @@
 namespace LaravelToolkit\Enum;
 
 use Exception;
+use LaravelToolkit\Actions\Mask\MaskNumber;
+use LaravelToolkit\Support\RegexTools;
 
 enum Phone: string implements ArrayableEnum
 {
-    use HasArrayableEnum;
+    use HasArrayableEnum, RegexTools;
 
     case LANDLINE = 'landline';
     case LOCAL_FARE = 'local_fare';
@@ -15,8 +17,21 @@ enum Phone: string implements ArrayableEnum
     case PUBLIC_SERVICES = 'public_services';
     case GENERIC = 'generic';
 
+    public static function guessType(string $number): ?Phone
+    {
+        return match (true) {
+            self::MOBILE->appearsToBe($number) => self::MOBILE,
+            self::LOCAL_FARE->appearsToBe($number) => self::LOCAL_FARE,
+            self::NON_REGIONAL->appearsToBe($number) => self::NON_REGIONAL,
+            self::LANDLINE->appearsToBe($number) => self::LANDLINE,
+            self::PUBLIC_SERVICES->appearsToBe($number) => self::PUBLIC_SERVICES,
+            default => null,
+        };
+    }
+
     public function appearsToBe(string $number): bool
     {
+        $this->regexOnlyNumbers($number);
         throw_if($this === self::GENERIC, Exception::class, 'Appears to be method is not allowed to generic type');
         $pattern = match ($this) {
             self::LANDLINE => '/^[1-9][0-9][1-5][0-9]$/',
@@ -35,16 +50,12 @@ enum Phone: string implements ArrayableEnum
 
     public function isValid(string $number): bool
     {
-        if ($this === self::GENERIC) {
-            return match (true) {
-                self::MOBILE->appearsToBe($number) => self::MOBILE->isValid($number),
-                self::LOCAL_FARE->appearsToBe($number) => self::LOCAL_FARE->isValid($number),
-                self::NON_REGIONAL->appearsToBe($number) => self::NON_REGIONAL->isValid($number),
-                self::LANDLINE->appearsToBe($number) => self::LANDLINE->isValid($number),
-                self::PUBLIC_SERVICES->appearsToBe($number) => self::PUBLIC_SERVICES->isValid($number),
-            };
+        $this->regexOnlyNumbers($number);
+        $type = $this === self::GENERIC ? self::guessType($number) : $this;
+        if ($type === null) {
+            return false;
         }
-        $pattern = match ($this) {
+        $pattern = match ($type) {
             self::LANDLINE => '/^[1-9][0-9][1-5][0-9]{7}$/',
             self::LOCAL_FARE => '/^400[0-9]{5}$/',
             self::MOBILE => '/^[1-9][0-9]9[5-9][0-9]{7}$/',
@@ -53,5 +64,34 @@ enum Phone: string implements ArrayableEnum
         };
 
         return preg_match($pattern, $number) === 1;
+    }
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::LANDLINE => 'Fixo',
+            self::LOCAL_FARE => 'Tarifa local',
+            self::MOBILE => 'Móvel',
+            self::NON_REGIONAL => 'Não regional',
+            self::PUBLIC_SERVICES => 'Serviço publico',
+            self::GENERIC => 'Genérico',
+        };
+    }
+
+    public function mask(string $number): string
+    {
+        $this->regexOnlyNumbers($number);
+        $type = $this === self::GENERIC ? self::guessType($number) : $this;
+        if ($type === null) {
+            return $number;
+        }
+        $handler = app(MaskNumber::class);
+        return match ($type) {
+            self::LANDLINE => $handler->handle('(##) ####-####', $number),
+            self::LOCAL_FARE => $handler->handle('####-####', $number),
+            self::MOBILE => $handler->handle('(##) # ####-####', $number),
+            self::NON_REGIONAL => $handler->handle('####-###-####', $number),
+            self::PUBLIC_SERVICES => $handler->handle('###', $number),
+        };
     }
 }
---
+++ tests/Enum/PhoneTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use LaravelToolkit\Enum\Phone;
+
+it('can guess from number', function () {
+    expect(Phone::guessType('4132324545'))
+        ->toBe(Phone::LANDLINE)
+        ->and(Phone::guessType('40031234'))
+        ->toBe(Phone::LOCAL_FARE)
+        ->and(Phone::guessType('32988232323'))
+        ->toBe(Phone::MOBILE)
+        ->and(Phone::guessType('08001235432'))
+        ->toBe(Phone::NON_REGIONAL)
+        ->and(Phone::guessType('123'))
+        ->toBe(Phone::PUBLIC_SERVICES);
+
+});
+
+it('can validate', function () {
+    expect(Phone::LANDLINE->isValid('4132324545'))
+        ->toBeTrue()
+        ->and(Phone::LANDLINE->isValid('4192324545'))
+        ->toBeFalse()
+        ->and(Phone::LOCAL_FARE->isValid('40031234'))
+        ->toBeTrue()
+        ->and(Phone::LOCAL_FARE->isValid('39991234'))
+        ->toBeFalse()
+        ->and(Phone::MOBILE->isValid('32988232323'))
+        ->toBeTrue()
+        ->and(Phone::MOBILE->isValid('32948232323'))
+        ->toBeFalse()
+        ->and(Phone::NON_REGIONAL->isValid('08001235432'))
+        ->toBeTrue()
+        ->and(Phone::NON_REGIONAL->isValid('00001235432'))
+        ->toBeFalse()
+        ->and(Phone::PUBLIC_SERVICES->isValid('123'))
+        ->toBeTrue()
+        ->and(Phone::PUBLIC_SERVICES->isValid('1223'))
+        ->toBeFalse();
+
+});
+
+it('can mask', function () {
+    expect(Phone::LANDLINE->mask('4132324545'))
+        ->toEqual('(41) 3232-4545')
+        ->and(Phone::LOCAL_FARE->mask('40012132'))
+        ->toEqual('4001-2132')
+        ->and(Phone::MOBILE->mask('43988762432'))
+        ->toEqual('(43) 9 8876-2432')
+        ->and(Phone::NON_REGIONAL->mask('08003221234'))
+        ->toEqual('0800-322-1234')
+        ->and(Phone::PUBLIC_SERVICES->mask('180'))
+        ->toEqual('180')
+        ->and(Phone::GENERIC->mask('4132324545'))
+        ->toEqual('(41) 3232-4545')
+        ->and(Phone::GENERIC->mask('40012132'))
+        ->toEqual('4001-2132')
+        ->and(Phone::GENERIC->mask('43988762432'))
+        ->toEqual('(43) 9 8876-2432')
+        ->and(Phone::GENERIC->mask('180'))
+        ->toEqual('180')
+        ->and(Phone::GENERIC->mask('1'))
+        ->toEqual('1');
+});
+
+it('can get label', function (Phone $phone) {
+    expect($phone->label())
+        ->toBeString();
+})->with(Phone::cases());
