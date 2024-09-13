8781796b2bbd9272e52f8c2584c57f6a4a76cc85
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2024-09-12 21:51:43 -0300

    wip

--- tests/Sitemap/IndexTest.php
+++ tests/Sitemap/IndexTest.php
@@ -4,6 +4,8 @@ use LaravelToolkit\Sitemap\Index;
 use Saloon\XmlWrangler\Data\Element;
 
 it('can instantiate', function () {
+    $this->artisan('laraveltoolkit:install_sitemap --force')
+        ->expectsOutputToContain('Published Sitemap routes file.');
     $url = new Index('users');
     expect($url)
         ->toBeInstanceOf(Index::class)
