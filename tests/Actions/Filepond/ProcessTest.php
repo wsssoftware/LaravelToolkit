2358e99a54024d5d7f1e7bbc117376893880f08c
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2024-09-18 18:56:12 -0300

    wip

--- tests/Actions/Filepond/ProcessTest.php
+++ tests/Actions/Filepond/ProcessTest.php
@@ -6,5 +6,6 @@ use LaravelToolkit\Facades\Filepond;
 it('can process a normal file', function () {
     $response = $this->post(route('lt.filepond.process'), ['filepond' => UploadedFile::fake()->image('image.jpg')]);
 
+
     Filepond::disk()->assertExists(Filepond::path($response->content(), 'image.jpg'));
 });
