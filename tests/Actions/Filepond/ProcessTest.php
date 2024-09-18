55c04e2e8aae8950d23a043cfa2061a2b78fa62c
allanmcarvalho <allanmcarvalho@users.noreply.github.com>
2024-09-18 21:56:38 +0000

    Fix styling

--- tests/Actions/Filepond/ProcessTest.php
+++ tests/Actions/Filepond/ProcessTest.php
@@ -6,6 +6,5 @@ use LaravelToolkit\Facades\Filepond;
 it('can process a normal file', function () {
     $response = $this->post(route('lt.filepond.process'), ['filepond' => UploadedFile::fake()->image('image.jpg')]);
 
-
     Filepond::disk()->assertExists(Filepond::path($response->content(), 'image.jpg'));
 });
