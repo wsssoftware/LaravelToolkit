ab3f978b22ab864e691f38a12bfe17d8ecf8ed84
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2024-09-19 16:50:30 -0300

    tests

---
+++ tests/Actions/Filepond/RestoreTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use LaravelToolkit\Facades\Filepond;
+
+it('can restore a file', function () {
+    Storage::fake(Filepond::diskName());
+    $id = Str::uuid()->toString();
+    Filepond::disk()->put(Filepond::path($id, 'foo.zip'), 'content example');
+
+    $this->get(route('lt.filepond.restore', ['id' => $id]))
+        ->assertSuccessful()
+        ->assertContent('content example')
+        ->assertHeader('Content-Disposition', 'inline')
+        ->assertHeader('Content-Type', 'application/zip')
+        ->assertHeader('Filename', 'foo.zip');
+});
