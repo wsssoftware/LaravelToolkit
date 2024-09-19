7c22dc9b04627cf296cf29de5c274aff08008cba
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2024-09-19 16:52:00 -0300

    fix tests

--- tests/Actions/Filepond/RestoreTest.php
+++ tests/Actions/Filepond/RestoreTest.php
@@ -5,12 +5,12 @@ use LaravelToolkit\Facades\Filepond;
 it('can restore a file', function () {
     Storage::fake(Filepond::diskName());
     $id = Str::uuid()->toString();
-    Filepond::disk()->put(Filepond::path($id, 'foo.zip'), 'content example');
+    Filepond::disk()->put(Filepond::path($id, 'foo.jpg'), 'content example');
 
     $this->get(route('lt.filepond.restore', ['id' => $id]))
         ->assertSuccessful()
         ->assertContent('content example')
         ->assertHeader('Content-Disposition', 'inline')
-        ->assertHeader('Content-Type', 'application/zip')
+        ->assertHeader('Content-Type', 'application/jpeg')
         ->assertHeader('Filename', 'foo.zip');
 });
