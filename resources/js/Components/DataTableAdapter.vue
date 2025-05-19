d3a2a635b4d6d9ecaa06b93b8f9b79fffdf2e1dc
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2025-05-19 13:58:57 -0300

    Enhance DataTableAdapter with filter and sort persistence
    Added support for persisting filters and sort options in `DataTableAdapter` using cookies. Updated dependencies in `package.json` to include `js-cookie` and its type definitions to enable this feature.

--- resources/js/Components/DataTableAdapter.vue
+++ resources/js/Components/DataTableAdapter.vue
@@ -1,139 +1,205 @@
 <template>
-  <slot
-      :loading="loading"
-      :filter="onFilter"
-      :manual-filter="onManualFilter"
-      :page="onPage"
-      :rows="paginator?.per_page"
-      :sort="onSort"
-      :total="paginator?.total"
-      :value="paginator?.data ?? []"/>
+    <slot
+        :currentSort="sort ?? []"
+        :loading="loading"
+        :filter="onFilter"
+        :manual-filter="onManualFilter"
+        :page="onPage"
+        :rows="paginator?.per_page"
+        :sort="onSort"
+        :total="paginator?.total"
+        :value="paginator?.data ?? []"/>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, nextTick, PropType} from "vue";
 import {Paginator} from "../index";
 import {
-  DataTableFilterEvent,
-  DataTableFilterMeta,
-  DataTablePageEvent,
-  DataTableSortEvent,
-  DataTableSortMeta
+    DataTableFilterEvent,
+    DataTableFilterMeta,
+    DataTablePageEvent,
+    DataTableSortEvent,
+    DataTableSortMeta
 } from "primevue/datatable";
 import {router} from "@inertiajs/vue3";
 import {debounce} from "lodash-es";
+import Cookies from 'js-cookie'
 
 export default defineComponent({
-  name: "DataTableAdapter",
-  props: {
-    globalFilterName: {
-      type: String,
-      default: "global"
+    name: "DataTableAdapter",
+    props: {
+        filters: Object as PropType<DataTableFilterMeta>,
+        globalFilterName: {
+            type: String,
+            default: "global"
+        },
+        manualFilterDebounceWait: {
+            type: Number,
+            default: 700,
+        },
+        propName: {
+            type: String,
+            required: true,
+        },
+        rememberFiltersKey: String,
+        rememberSortKey: String,
+        rows: {
+            type: Number,
+            default: 15
+        }
     },
-    manualFilterDebounceWait: {
-      type: Number,
-      default: 700,
+    emits: ['update:filters'],
+    computed: {
+        fRememberFiltersKey(): string | undefined {
+            if (this.rememberFiltersKey === undefined) {
+                return undefined
+            }
+            return `datatable_filters_${this.rememberFiltersKey}`
+        },
+        fRememberSortKey(): string | undefined {
+            if (this.rememberSortKey === undefined) {
+                return undefined
+            }
+            return `datatable_sort_${this.rememberSortKey}`
+        },
+        localFilters: {
+            get(): DataTableFilterMeta {
+                return this.filters ?? this.lFilters
+            },
+            set(value: DataTableFilterMeta) {
+                if (this.rememberFiltersKey) {
+                    let expire = new Date();
+                    expire.setHours(expire.getHours() + 2);
+                    Cookies.set(
+                        this.fRememberFiltersKey,
+                        JSON.stringify(value),
+                        {expires: expire}
+                    )
+                }
+                this.lFilters = value;
+                this.$emit('update:filters', value)
+            },
+        },
+        paginator(): Paginator<any> | undefined {
+            return this.$page.props[this.propName] as Paginator<any> | undefined
+        },
+        pageName(): string {
+            return this.paginator?.page_name ?? 'page';
+        }
     },
-    propName: {
-      type: String,
-      required: true,
+    data() {
+        return {
+            lFilters: {} as DataTableFilterMeta,
+            debouncedFilter: debounce((filters: any) => {
+                this.localFilters = filters
+                this.load()
+            }, this.manualFilterDebounceWait),
+            loading: false,
+            page: 1,
+            sort: [] as DataTableSortMeta[] | undefined,
+        }
     },
-    rows: {
-      type: Number,
-      default: 15
-    }
-  },
-  computed: {
-    paginator(): Paginator<any> | undefined {
-      return this.$page.props[this.propName] as Paginator<any> | undefined
+    beforeMount() {
+        if (this.rememberFiltersKey) {
+            try {
+                this.localFilters = JSON.parse(Cookies.get(this.fRememberFiltersKey));
+            } catch (e) {
+                //
+            }
+        }
+        if (this.rememberSortKey) {
+            try {
+                this.sort = JSON.parse(Cookies.get(this.fRememberSortKey));
+            } catch (e) {
+                //
+            }
+        }
+        if (this.paginator === undefined) {
+            this.load();
+        }
     },
-    pageName(): string {
-      return this.paginator?.page_name ?? 'page';
-    }
-  },
-  data() {
-    return {
-      debouncedFilter: debounce((filters: any) => {
-        this.filters = filters
-        this.load()
-      }, this.manualFilterDebounceWait),
-      filters: {} as DataTableFilterMeta,
-      loading: false,
-      page: 1,
-      sort: [] as DataTableSortMeta[] | undefined,
-    }
-  },
-  beforeMount() {
-    if (this.paginator === undefined) {
-      this.load();
-    }
-  },
-  beforeUpdate() {
-    if (this.paginator === undefined) {
-      this.load();
-    }
-  },
-  methods: {
-    onFilter(event: DataTableFilterEvent) {
-      this.filters = event.filters
-      this.load();
+    beforeUpdate() {
+        if (this.paginator === undefined) {
+            this.load();
+        }
     },
-    onManualFilter(filters: DataTableFilterMeta) {
-      this.debouncedFilter(filters)
+    methods: {
+        onFilter(event: DataTableFilterEvent) {
+            this.localFilters = event.filters;
+            this.load();
+        },
+        onManualFilter(filters: DataTableFilterMeta) {
+            this.debouncedFilter(filters)
+        },
+        onPage(event: DataTablePageEvent) {
+            this.page = event.page + 1
+        },
+        onSort(event: DataTableSortEvent) {
+            if (event.sortField !== null) {
+                this.sort = [{field: event.sortField, order: event.sortOrder}]
+            } else {
+                this.sort = event.multiSortMeta
+            }
+            if (this.rememberSortKey) {
+                if (this.sort.length > 0) {
+                    let expire = new Date();
+                    expire.setHours(expire.getHours() + 2);
+                    Cookies.set(
+                        this.fRememberSortKey,
+                        JSON.stringify(this.sort),
+                        {expires: expire}
+                    )
+                } else {
+                    Cookies.remove(this.fRememberSortKey)
+                }
+            }
+        },
+        load(): void {
+            this.loading = true
+            let data: { [key: string]: any } = {}
+            let sort = undefined
+            if (this.sort && this.sort.length > 0) {
+                sort = this.sort?.map((i: DataTableSortMeta) => `${i.field}:${i.order === 1 ? 'asc' : 'desc'}`).join(',')
+            }
+            nextTick(() => {
+                data[`${this.pageName}-options`] = {
+                    rows: this.rows,
+                    sort: sort,
+                    filters: Object.keys(this.localFilters).length > 0 ? this.localFilters : undefined,
+                    global_filter_name: this.globalFilterName,
+                }
+                data[this.pageName] = undefined;
+                if (this.page !== 1) {
+                    data[this.pageName] = this.page
+                }
+                router.cancelAll();
+                router.reload({
+                    method: 'post',
+                    only: [this.propName],
+                    data: data,
+                    replace: true,
+                    async: false,
+                    onSuccess: () => this.loading = false
+                })
+            })
+
+        }
     },
-    onPage(event: DataTablePageEvent) {
-      this.page = event.page + 1
+    watch: {
+        page() {
+            this.load()
+        },
+        rows() {
+            this.page = 1
+            this.load()
+        },
+        sort: {
+            deep: true,
+            handler() {
+                this.load()
+            }
+        },
     },
-    onSort(event: DataTableSortEvent) {
-      if (event.sortField !== null) {
-        this.sort = [{field: event.sortField, order: event.sortOrder}]
-      } else {
-        this.sort = event.multiSortMeta
-      }
-    },
-    load(): void {
-      this.loading = true
-      let data: { [key: string]: any } = {}
-      let sort = undefined
-      if (this.sort && this.sort.length > 0) {
-        sort = this.sort?.map((i: DataTableSortMeta) => `${i.field}:${i.order === 1 ? 'asc' : 'desc'}`).join(',')
-      }
-      data[`${this.pageName}-options`] = {
-        rows: this.rows,
-        sort: sort,
-        filters: Object.keys(this.filters).length > 0 ? this.filters : undefined,
-        global_filter_name: this.globalFilterName,
-      }
-      data[this.pageName] = undefined;
-      if (this.page !== 1) {
-        data[this.pageName] = this.page
-      }
-      router.cancelAll();
-      router.reload({
-        method: 'post',
-        only: [this.propName],
-        data: data,
-        replace: true,
-        async: false,
-        onSuccess: () => this.loading = false
-      })
-    }
-  },
-  watch: {
-    page() {
-      this.load()
-    },
-    rows() {
-      this.page = 1
-      this.load()
-    },
-    sort: {
-      deep: true,
-      handler() {
-        this.load()
-      }
-    },
-  },
 })
 </script>
 
